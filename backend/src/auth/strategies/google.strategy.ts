c819e467ec687ed5b0fdcfd22701fad11c03daa5
kingxinjae <zz4131@naver.com>
2026-05-18 23:02:07 +0900

    session:false 주석처리

--- backend/src/auth/strategies/google.strategy.ts
+++ backend/src/auth/strategies/google.strategy.ts
@@ -15,7 +15,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET')!,
       callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL')!,
       scope: ['email', 'profile'],
-      session: false,
+      //session: false,
     });
   }
 
